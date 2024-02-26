177e1474391ed28c853c23257e4006d697f579b1
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-02-26 18:29:17 -0300

    aula07

---
+++ src/aula07.ts
@@ -0,0 +1,19 @@
+const funcaoCallback = (valor: number): void => {
+   console.log("Funcionou " + valor);
+};
+
+const funcaoTeste2 = (
+   valor1: number,
+   valor2: number,
+   callback: (valor: number) => void,
+): string => {
+   if (valor1 > valor2) {
+      callback(valor1);
+   }
+   return "";
+};
+
+funcaoTeste2(523, 32, funcaoCallback);
+funcaoTeste2(43, 32, (x: number) => {
+   console.log("outra função " + x);
+});
